1d213f35d5daae18524ded2f1d6e3dcbda26f651
Szymon <majewskis@gmail.com>
2016-09-30 23:24:45 -0700

    added trail to superhero

---
+++ Assets/TrailRendererSetup.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrailRendererSetup : MonoBehaviour
+{
+    private void Awake()
+    {
+        var trailRenderer = GetComponent<TrailRenderer>();
+        trailRenderer.sortingOrder = 49;
+    }
+}
