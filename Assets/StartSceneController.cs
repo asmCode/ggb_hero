d64b612310faacea5800eab7741b7510668d8118
Szymon <majewskis@gmail.com>
2016-09-30 23:35:27 -0700

    another try to fix story timing

---
+++ Assets/StartSceneController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartSceneController : MonoBehaviour
+{
+    private void Start()
+    {
+        SceneManager.LoadScene("IntroScene");
+    }
+}
