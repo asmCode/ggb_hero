9599f07b8218face0b0ac69200ed28c6492764a2
Majak <majakthecoder@gmail.com>
2016-10-05 00:36:17 -0700

    fixed social code for iOS

--- Assets/Scenes/GameplayTest/Scripts/Social/SocialImplIOS.cs
+++ Assets/Scenes/GameplayTest/Scripts/Social/SocialImplIOS.cs
@@ -1,16 +1,17 @@
-﻿#if UNITY_IOS && !UNITY_EDITOR
+﻿//#if UNITY_IOS && !UNITY_EDITOR
+#if UNITY_IOS
 
 namespace Ssg.Social
 {
     public class SocialImplIOS : ISocialImpl
     {
-        private UnityEngine.SocialPlatforms.Social.ILeaderboard m_leaderboardSuisSaved;
+		private UnityEngine.SocialPlatforms.ILeaderboard m_leaderboardSuisSaved;
 
         public bool IsAuthenticated
         {
             get
             {
-                return UnityEngine.SocialPlatforms.Social.localUser.authenticated;
+				return UnityEngine.Social.localUser.authenticated;
             }
         }
 
@@ -21,32 +22,33 @@ namespace Ssg.Social
 
         public void GetLocalUserScore(string leaderboardId, System.Action<Score> callback)
         {
-if (m_leaderboardSuisSaved == null)
-{
-m_leaderboardSuisSaved = Social.CreateLeaderboard();
-		    m_leaderboardSuisSaved.id = leaderboardId;
-            
-}
+			if (m_leaderboardSuisSaved == null)
+			{
+				m_leaderboardSuisSaved = UnityEngine.Social.CreateLeaderboard();
+				m_leaderboardSuisSaved.id = leaderboardId;      
+			}
 
-m_leaderboardSuisSaved.SetUserFilter(new string[] { UnityEngine.SocialPlatforms.Social.localUser.id });
+			m_leaderboardSuisSaved.SetUserFilter(new string[] {UnityEngine.Social.localUser.id});
             
-		    m_leaderboardSuisSaved.LoadScores(result => {
-    bool success = m_leaderboardSuisSaved != null && m_leaderboardSuisSaved.scores != null && m_leaderboardSuisSaved.scores.Length == 1;
-    Score score = null;
-if (success)
-{
-    score = new Score();
-score.Value = m_leaderboardSuisSaved.scores[0].value;
-score.Rank = m_leaderboardSuisSaved.scores[0].rank;
-}
-        if (callback != null)
-                callback(score);
-            });
+		    m_leaderboardSuisSaved.LoadScores(result =>
+			{
+	    		bool success = m_leaderboardSuisSaved != null && m_leaderboardSuisSaved.scores != null && m_leaderboardSuisSaved.scores.Length == 1;
+			    Score score = null;
+				if (success)
+				{
+				    score = new Score();
+					score.Value = m_leaderboardSuisSaved.scores[0].value;
+					score.Rank = m_leaderboardSuisSaved.scores[0].rank;
+				}
+
+		        if (callback != null)
+		        	callback(score);
+			});
         }
 
         public void ReportLocalUserScore(string leaderboardId, long score, System.Action<bool> callback)
         {
-            UnityEngine.SocialPlatforms.Social.ReportScore(score, leaderboardID, success =>
+            UnityEngine.Social.ReportScore(score, leaderboardId, success =>
             {
                 if (callback != null)
                     callback(success);
