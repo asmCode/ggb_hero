154297f460110359bfd596e33093320caf4a7ea1
Szymon <majewskis@gmail.com>
2017-04-03 23:25:45 -0700

    replacing assets

--- Assets/Scripts/SmoothCamera.cs
+++ Assets/Scripts/SmoothCamera.cs
@@ -8,38 +8,34 @@ public class SmoothCamera : MonoBehaviour
     public RectBounds m_cameraSafeArea;
 
     private Vector3 m_velocity;
-    private Vector2 m_bounds;
+    private Vector2 m_cameraPositionConstrains;
 
     private void Start()
     {
-        /*
-        CameraAutoRatio cameraAutoRato = GetComponent<CameraAutoRatio>();
-        cameraAutoRato.SetSize();
         Camera camera = GetComponent<Camera>();
-        float aspect = (float)Screen.width / Screen.height;
-        float camSizeDiffX = CameraAutoRatio.FullCameraSize * (4.0f / 3.0f) - camera.orthographicSize * aspect;
-        float camSizeDiffY = CameraAutoRatio.FullCameraSize - camera.orthographicSize;
-        m_bounds = new Vector2(camSizeDiffX, camSizeDiffY);
-        */
+        float camWidth = camera.orthographicSize * camera.aspect * 2;
+        float camHeight = camera.orthographicSize * 2;
 
-        Camera camera = GetComponent<Camera>();
-        float camWidth = camera.orthographicSize * camera.aspect;
-        float camHeight = camera.orthographicSize;
-
-        m_bounds = new Vector2();
-        m_bounds.x = camWidth - m_cameraSafeArea.GetBounds().max.x;
-        m_bounds.y = camHeight - m_cameraSafeArea.GetBounds().max.y;
+        m_cameraPositionConstrains = new Vector2();
+        // We are assuming, that camera is bigger than safe area
+        m_cameraPositionConstrains.x = camWidth - m_cameraSafeArea.GetBounds().size.x;
+        m_cameraPositionConstrains.y = camHeight - m_cameraSafeArea.GetBounds().size.y;
     }
 
     private void Update()
     {
         Vector3 position = transform.position;
-        //position = Vector3.SmoothDamp(position, m_objectToFollow.position, ref m_velocity, 0.1f);
-        //position.z = transform.position.z;
+        
+        Bounds objectToFollowBounds = m_objectToFollowBounds.GetBounds();
+        Bounds cameraSafeAreaBounds = m_cameraSafeArea.GetBounds();
 
-        Bounds bounds = m_objectToFollowBounds.GetBounds();
-        position.x = Mathf.Lerp(-m_bounds.x, m_bounds.x, (m_objectToFollow.position.x - bounds.min.x) / bounds.size.x);
-        position.y = Mathf.Lerp(-m_bounds.y, m_bounds.y, (m_objectToFollow.position.y - bounds.min.y) / bounds.size.y);
+        float maxLeftCamPos = -m_cameraPositionConstrains.x / 2 + cameraSafeAreaBounds.center.x;
+        float maxRightCamPos = m_cameraPositionConstrains.x / 2 + cameraSafeAreaBounds.center.x;
+        float maxBottomCamPos = -m_cameraPositionConstrains.y / 2 + cameraSafeAreaBounds.center.y;
+        float maxTopCamPos = m_cameraPositionConstrains.y / 2 + cameraSafeAreaBounds.center.y;
+
+        position.x = Mathf.Lerp(maxLeftCamPos, maxRightCamPos, Mathf.Clamp01((m_objectToFollow.position.x - objectToFollowBounds.min.x) / objectToFollowBounds.size.x));
+        position.y = Mathf.Lerp(maxBottomCamPos, maxTopCamPos, Mathf.Clamp01((m_objectToFollow.position.y - objectToFollowBounds.min.y) / objectToFollowBounds.size.y));
         position.z = transform.position.z;
 
         transform.position = Vector3.SmoothDamp(transform.position, position, ref m_velocity, 0.4f);
